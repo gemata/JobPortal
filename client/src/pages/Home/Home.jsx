37d33e81df10e374b08619a46b24583c9a279c8e
Bleon Jusufi <bjusufi7@gmail.com>
2024-04-14 23:20:22 +0200

    Added another section to Home Page

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,10 +1,39 @@
 import React from "react";
 import SearchSection from "../../components/Search/SearchSection";
+import "./style.css";
 
 const Home = () => {
   return (
     <div className="homepage">
       <SearchSection />
+      <div className="help-section">
+        <div className="help-section__wrapper">
+          <div className="img">
+            <img
+              src="https://image.cnbcfm.com/api/v1/image/107069627-1654086975733-gettyimages-143071319-pe0080780.jpeg?v=1688154770"
+              alt="graduate"
+            />
+          </div>
+          <ul className="help-list">
+            <div className="help-section__title">
+              Just graduated? We can help
+            </div>
+
+            <li>
+              <a href="">Visit the new grad hub</a>
+            </li>
+            <li>
+              <a href="">Search salaries by major</a>
+            </li>
+            <li>
+              <a href="">Get a free resume assessment</a>
+            </li>
+            <li>
+              <a href="">Search entry-level jobs</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
