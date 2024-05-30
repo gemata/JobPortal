1668c70f086f1bc43e73a60d12a14213dd8d2696
arjanit-ubt <129512947+arjanit-ubt@users.noreply.github.com>
2024-05-30 15:14:21 +0200

    Added More fields to Job List and added functionality to changing job status

--- client/src/components/CompanyComponents/DashboardNavSection.jsx
+++ client/src/components/CompanyComponents/DashboardNavSection.jsx
@@ -63,14 +63,14 @@ export default function DashboardNavSection() {
             </li>
             <li>
               <NavLink
-                to='/company/candidates'
+                to='/company/applicantlist/'
                 className={({ isActive }) => `flex gap-2 p-5 items-center ${isActive ? 'border-b-2 border-jobportal-cyan text-jobportal-cyan' : ''}`}
               >
                 {/* Users SVG */}
                 <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' fill='currentColor' className='w-4 h-4 mt-[0.125rem]'>
                   <path d='M8.5 4.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0ZM10.9 12.006c.11.542-.348.994-.9.994H2c-.553 0-1.01-.452-.902-.994a5.002 5.002 0 0 1 9.803 0ZM14.002 12h-1.59a2.556 2.556 0 0 0-.04-.29 6.476 6.476 0 0 0-1.167-2.603 3.002 3.002 0 0 1 3.633 1.911c.18.522-.283.982-.836.982ZM12 8a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z' />
                 </svg>
-                Candidates
+                Applicants
               </NavLink>
             </li>
             <li>
