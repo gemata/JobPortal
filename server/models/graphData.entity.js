f1604317a70769d55b68e9663cc289a1733797e2
DionKelmendi <dionkelmendi2003@gmail.com>
2024-06-05 15:13:10 +0200

    Added all graph data columns

--- server/models/graphData.entity.js
+++ server/models/graphData.entity.js
@@ -9,86 +9,249 @@ const GraphData = sequelize.define(
       autoIncrement: true,
       primaryKey: true,
     },
-    is_Active: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: true,
-      allowNull: false,
-    },
-    startAt: {
-      type: DataTypes.DATE,
-      allowNull: false,
-    },
-    endAt: {
-      type: DataTypes.DATE,
-      allowNull: false,
-    },
-    interviewMethod: {
-      type: DataTypes.STRING(255),
-      allowNull: false,
-    },
-    salary_from: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      validate: {
-        min: 1,
-        max: 999999,
-      },
-    },
-    salary_to: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      validate: {
-        min: 1,
-        max: 999999,
-      },
-    },
-    jobLocation: {
-      type: DataTypes.STRING(255),
-      allowNull: false,
-    },
-    education: {
-      type: DataTypes.STRING(255),
-      allowNull: true,
-    },
-    nationality: {
-      type: DataTypes.STRING(255),
-      allowNull: true,
-    },
-    likes: {
+    userCount: {
       type: DataTypes.INTEGER,
       defaultValue: 0,
-      allowNull: true,
     },
-
-    nrApplicants: {
+    companyCount: {
       type: DataTypes.INTEGER,
       defaultValue: 0,
-      allowNull: true,
     },
-    interviewActive: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-      allowNull: false,
-    },
-    interviewStart: {
-      type: DataTypes.DATE,
-      defaultValue: false,
-      allowNull: false,
-    },
-    interviewAddress: {
-      type: DataTypes.STRING(255),
-      defaultValue: false,
-      allowNull: true,
-    },
-    interviewLink: {
-      type: DataTypes.STRING(255),
-      defaultValue: false,
-      allowNull: true,
-    },
-    interviewNrCandidates: {
+    pendingAccountCount: {
       type: DataTypes.INTEGER,
       defaultValue: 0,
-      allowNull: false,
+    },
+    jobPostingsJan: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    jobPostingsFeb: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    jobPostingsMar: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    jobPostingsApr: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    jobPostingsMay: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    jobPostingsJun: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    userRegistrationsJan: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    userRegistrationsFeb: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    userRegistrationsMar: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    userRegistrationsApr: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    userRegistrationsMay: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    userRegistrationsJun: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    jobApplicationStatusApplied: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    jobApplicationStatusRejected: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    userEngagementLoginJan: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    userEngagementLoginFeb: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    userEngagementLoginMar: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    userEngagementLoginApr: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    userEngagementLoginMay: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    userEngagementLoginJun: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    userEngagementProfileJan: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    userEngagementProfileFeb: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    userEngagementProfileMar: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    userEngagementProfileApr: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    userEngagementProfileMay: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    userEngagementProfileJun: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    userEngagementResumeJan: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    userEngagementResumeFeb: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    userEngagementResumeMar: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    userEngagementResumeApr: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    userEngagementResumeMay: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    userEngagementResumeJun: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    subscriptionRevenueBasic: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    subscriptionRevenueStandard: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    subscriptionRevenuePremium: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    interviewsScheduledJan: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    interviewsScheduledFeb: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    interviewsScheduledMar: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    interviewsScheduledApr: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    interviewsScheduledMay: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    interviewsScheduledJun: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    jobPostEngagementLikes: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    jobPostEngagementShares: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    jobPostEngagementViews: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    jobFieldsPopular1: {
+      type: DataTypes.STRING,
+    },
+    jobFieldsPopular1Count: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    jobFieldsPopular2: {
+      type: DataTypes.STRING,
+    },
+    jobFieldsPopular2Count: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    jobFieldsPopular3: {
+      type: DataTypes.STRING,
+    },
+    jobFieldsPopular3Count: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    jobFieldsPopular4: {
+      type: DataTypes.STRING,
+    },
+    jobFieldsPopular4Count: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    topCompany1: {
+      type: DataTypes.STRING,
+    },
+    topCompany1Count: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    topCompany2: {
+      type: DataTypes.STRING,
+    },
+    topCompany2Count: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    topCompany3: {
+      type: DataTypes.STRING,
+    },
+    topCompany3Count: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    topCompany4: {
+      type: DataTypes.STRING,
+    },
+    topCompany4Count: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
     },
   },
   {
